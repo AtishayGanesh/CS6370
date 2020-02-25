4386c0c44893536e35a526bd80449145d9394b24
AtishayGanesh <atishay012@gmail.com>
2020-02-25 11:52:58 +0530

    Added Lemmatization

--- Assignments/src/inflectionReduction.py
+++ Assignments/src/inflectionReduction.py
@@ -6,28 +6,38 @@ from util import *
 
 
 class InflectionReduction:
+    def __init__(self):
+        self.lemmatizer = nltk.stem.WordNetLemmatizer()
+    def reduce(self, text):
+        """
+        Stemming/Lemmatization
 
-	def reduce(self, text):
-		"""
-		Stemming/Lemmatization
+        Parameters
+        ----------
+        arg1 : list
+            A list of lists where each sub-list a sequence of tokens
+            representing a sentence
 
-		Parameters
-		----------
-		arg1 : list
-			A list of lists where each sub-list a sequence of tokens
-			representing a sentence
+        Returns
+        -------
+        list
+            A list of lists where each sub-list is a sequence of
+            stemmed/lemmatized tokens representing a sentence
+        """
 
-		Returns
-		-------
-		list
-			A list of lists where each sub-list is a sequence of
-			stemmed/lemmatized tokens representing a sentence
-		"""
-
-		reducedText = None
-
-		#Fill in code here
-		
-		return reducedText
+        reducedText = []
+        print(self.lemmatizer.lemmatize('corpora'))
+        #raise AssertionError
+        #Fill in code here
+        for sentence in text:
+            reducedSentence = [self.lemmatizer.lemmatize(word)
+             for word in sentence]
+            reducedText.append(reducedSentence)
 
 
+
+        return reducedText
+if __name__=="__main__":
+    a = InflectionReduction()
+    b = [['my','names','are','corpora'],['her','women','eating','analysis','leaves','operations'],['to','being','desirable']]
+    print(a.reduce(b))
