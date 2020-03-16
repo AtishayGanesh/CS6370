2da4e1efbcd94663aa80133453c0b128e14add18
AtishayGanesh <atishay012@gmail.com>
2020-03-16 15:17:38 +0530

    Hi

--- Assignments/src/informationRetrieval.py
+++ Assignments/src/informationRetrieval.py
@@ -7,70 +7,70 @@ from util import *
 
 class InformationRetrieval():
 
-	def __init__(self):
-		self.index = None
+    def __init__(self):
+        self.index = None
 
-	def buildIndex(self, docs, docIDs):
-		"""
-		Builds the document index in terms of the document
-		IDs and stores it in the 'index' class variable
+    def buildIndex(self, docs, docIDs):
+        """
+        Builds the document index in terms of the document
+        IDs and stores it in the 'index' class variable
 
-		Parameters
-		----------
-		arg1 : list
-			A list of lists of lists where each sub-list is
-			a document and each sub-sub-list is a sentence of the document
-		arg2 : list
-			A list of integers denoting IDs of the documents
-		Returns
-		-------
-		None
-		"""
-		index = {}
-		doc_counter = 0
-		for document in docs:
-			for sentence in document:
-				for word in sentence:
-					if(index.has_key(word)):
-						flag = 0
-						for i in range(len(index[word])):
-							if(index[word][i][0] == docIDs[doc_counter]):
-								index[word][i][1] += 1
-								flag = 1
-						if(flag == 0):
-							index[word].append([docIDs[doc_counter], 1])
-					else:
-							index[word] = []
-							index[word].append([docIDs[doc_counter], 1])
-			doc_counter += 1
-		#Fill in code here
+        Parameters
+        ----------
+        arg1 : list
+            A list of lists of lists where each sub-list is
+            a document and each sub-sub-list is a sentence of the document
+        arg2 : list
+            A list of integers denoting IDs of the documents
+        Returns
+        -------
+        None
+        """
+        index = {}
+        doc_counter = 0
+        for document in docs:
+            for sentence in document:
+                for word in sentence:
+                    if(index.has_key(word)):
+                        flag = 0
+                        for i in range(len(index[word])):
+                            if(index[word][i][0] == docIDs[doc_counter]):
+                                index[word][i][1] += 1
+                                flag = 1
+                        if(flag == 0):
+                            index[word].append([docIDs[doc_counter], 1])
+                    else:
+                            index[word] = []
+                            index[word].append([docIDs[doc_counter], 1])
+            doc_counter += 1
+        #Fill in code here
 
-		self.index = index
+        self.index = index
 
 
-	def rank(self, queries):
-		"""
-		Rank the documents according to relevance for each query
+    def rank(self, queries):
+        """
+        Rank the documents according to relevance for each query
 
-		Parameters
-		----------
-		arg1 : list
-			A list of lists of lists where each sub-list is a query and
-			each sub-sub-list is a sentence of the query
-		
+        Parameters
+        ----------
+        arg1 : list
+            A list of lists of lists where each sub-list is a query and
+            each sub-sub-list is a sentence of the query
+        
 
-		Returns
-		-------
-		list
-			A list of lists of integers where the ith sub-list is a list of IDs
-			of documents in their predicted order of relevance to the ith query
-		"""
+        Returns
+        -------
+        list
+            A list of lists of integers where the ith sub-list is a list of IDs
+            of documents in their predicted order of relevance to the ith query
+        """
 
-		doc_IDs_ordered = []
+        doc_IDs_ordered = []
 
-		#Fill in code here
-	
-		return doc_IDs_ordered
+        #Fill in code here
+    
+        return doc_IDs_ordered
 
 
 
